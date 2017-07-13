a14554126a3e2e134d4d63362643e37fe5acc487
anggunasr <aanggunsr@gmail.com>
2017-07-14 04:09:17 +0700

    frontuser-001

--- src/app/beranda/beranda.component.ts
+++ src/app/beranda/beranda.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BerandaComponent implements OnInit {
 
+	messageClass;
+	message;
+	newPost = false;
+	loadDash = false;
+
   constructor() { }
+
+  newPostDash(){
+  	this.newPost = true;
+  }
+
+  reloadDash(){
+  	this.loadDash = true;
+
+  	//get all post
+  	setTimeout(() =>{
+  		this.loadDash = false;
+  	}, 3000);
+  }
+
+  draftComment(){
+  	
+  }
 
   ngOnInit() {
   }
